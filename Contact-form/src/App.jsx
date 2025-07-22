6c4c06e3e9026a227b5a2255b1234f9550b711ca
ahmed oueslati <oueslatiahmed38@gmail.com>
2025-07-22 09:44:40 +0200

    commit

--- Contact-form/src/App.jsx
+++ Contact-form/src/App.jsx
@@ -1,35 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import './App.css'
+import React, { useState } from "react";
+
+function ContactForm() {
+  
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    telephone: "",
+    message: "",
+  });
+
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prev => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+   
+    const { name, email, telephone, message } = formData;
+    if (!name || !email || !telephone || !message) {
+      alert("Please fill in all fields.");
+      return;
+    }
+
+   
+    console.log("Form Data Submitted:", formData);
+
+   
+  };
 
   return (
-    <>
+    <form onSubmit={handleSubmit}>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <label>Name:</label><br />
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <label>Email:</label><br />
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div>
+        <label>Telephone:</label><br />
+        <input
+          type="tel"
+          name="telephone"
+          value={formData.telephone}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label>Message:</label><br />
+        <textarea
+          name="message"
+          value={formData.message}
+          onChange={handleChange}
+        />
+      </div>
+      <button type="submit">Send</button>
+    </form>
+  );
 }
 
-export default App
+export default ContactForm;
